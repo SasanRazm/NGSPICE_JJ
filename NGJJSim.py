947298a8dd0767b95158e0b4e969cfd9c50aac9e
Sasan Razmkhah <39615471+SasanRazm@users.noreply.github.com>
2025-05-23 16:49:02 -0700

    Update NGJJSim.py

--- NGJJSim.py
+++ NGJJSim.py
@@ -5,7 +5,7 @@ import plotWaveform as PLW
 AddNoise = 0
 OrgNetlist = 'PI_AND_HighJc.cir'
 Out_file_path = 'PiOut.dat'
-dsets = [0,1,3,5,7,9] 
+dsets = [0,1,3,5] 
 
 RNG.OutputRM(Out_file_path)
 data = RNG.RunSpice(OrgNetlist, Out_file_path, AddNoise)
